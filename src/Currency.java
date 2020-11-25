ce41d6feddbdf41f8c340dc0d73f9d4e38634ab0
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2020-11-25 10:57:00 +0100

    implemented class Currency.java

--- src/Currency.java
+++ src/Currency.java
@@ -4,4 +4,24 @@
  */
 
 public class Currency {
+
+    public String sign;
+    public double value;
+
+    //getter and setter
+    public String getSign()
+    {
+        return sign;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    //constructor
+    public Currency(String s, double v)
+    {
+        this.sign = s;
+        this.value = v;
+    }
 }
